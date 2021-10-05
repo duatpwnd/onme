2c52c39ec6e6c617de299bb4afc5885594538f28
duatpwnd <duatpwnd@snaptag.co.kr>
2021-10-05 14:03:02 +0900

    애플 산돌 고딕NEO font-face 작성 및 reset.scss 세팅

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 <script lang="ts">
   import { computed, defineComponent, onUpdated, ref } from "vue";
-  import BaseCheckBox from "@/components/common/BaseCheckBox.vue";
   export default defineComponent({
     name: "Home",
     setup() {
@@ -16,24 +15,5 @@
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
+  @import "@/assets/reset.scss";
 </style>
