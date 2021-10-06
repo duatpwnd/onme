e775c8d920861703df076b9df27a4d01dc66fa60
duatpwnd <duatpwnd1@naver.com>
2021-10-06 16:45:37 +0900

    errorhandler, warnhander, axios 설정

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+import axios from "axios";
+const app = createApp(App);
+app.use(store).use(router).mount("#app");
+app.config.globalProperties.axios = axios;
+app.config.errorHandler = (err, vm, info) => {
+  console.log("에러:", err, "vm", vm, "info:", info);
+};
+app.config.warnHandler = (msg, vm, trace) => {
+  console.log("경고:", msg, vm, trace);
+};
