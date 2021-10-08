d75b8f64951ec6988065b9f27d53c12b7c709d28
duatpwnd <duatpwnd@snaptag.co.kr>
2021-10-08 18:47:28 +0900

    검색 탭 개발

---
+++ src/components/search-category/Tag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Tagwer</div>
+</template>
+<script lang="ts">
+  import {
+    defineComponent,
+    onMounted,
+    reactive,
+    ref,
+    getCurrentInstance,
+  } from "vue";
+  export default defineComponent({
+    name: "Tag",
+    setup() {
+      console.log("setup호출");
+      const instance = getCurrentInstance();
+      const axios = instance?.appContext.config.globalProperties.axios;
+      onMounted(() => {
+        console.log("onmounted호출");
+      });
+    },
+  });
+</script>
+<style scoped lang="scss"></style>
---
+++ src/views/Search.vue
@@ -0,0 +1,174 @@
+<template>
+  <button
+    class="back-btn"
+    v-show="searchTab"
+    @click="searchTab = false"
+  ></button>
+  <div class="search-area">
+    <input
+      type="text"
+      v-on:input="keyword = $event.target.value"
+      placeholder="작가, 태그 검색"
+      class="search-input"
+      :value="keyword"
+      @focus="searchTab = true"
+    />
+    <button
+      class="delete-btn"
+      @click="keyword = ''"
+      v-show="keyword.trim().length > 0"
+    >
+      지우기
+    </button>
+  </div>
+  <h2>추천</h2>
+  <div class="feed">
+    <div class="left-feed">
+      <router-link to="/detail"
+        ><img src="@/assets/images/feed1.png"
+      /></router-link>
+      <router-link to="/">
+        <img src="@/assets/images/feed5.png" />
+      </router-link>
+    </div>
+    <div class="right-feed">
+      <router-link to="/">
+        <img src="@/assets/images/feed2.png" />
+      </router-link>
+      <router-link to="/">
+        <img src="@/assets/images/feed3.png" />
+      </router-link>
+      <router-link to="/">
+        <img src="@/assets/images/feed4.png" />
+      </router-link>
+    </div>
+  </div>
+  <div v-show="searchTab" class="search-tab">
+    <button
+      v-for="(value, key) in type"
+      :key="key"
+      @click="currentComponent = key"
+      :class="[
+        'tab-btn',
+        { 'active-tab-btn': currentComponent.toLowerCase() == key },
+      ]"
+    >
+      {{ value }}
+    </button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+<script lang="ts">
+  import {
+    defineComponent,
+    onMounted,
+    reactive,
+    ref,
+    getCurrentInstance,
+  } from "vue";
+  import Total from "@/components/search-category/Total.vue";
+  import Writer from "@/components/search-category/Writer.vue";
+  import Tag from "@/components/search-category/Tag.vue";
+  export default defineComponent({
+    name: "Search",
+    components: {
+      Total,
+      Tag,
+      Writer,
+    },
+    setup() {
+      console.log("setup호출");
+      const instance = getCurrentInstance();
+      const axios = instance?.appContext.config.globalProperties.axios;
+      const keyword = ref("");
+      const currentComponent = ref("Total");
+      const searchTab = ref(false);
+      const type = {
+        total: "전체",
+        writer: "작가",
+        tag: "태그",
+      };
+      onMounted(() => {
+        console.log("onmounted호출");
+      });
+      return { keyword, type, currentComponent, searchTab };
+    },
+  });
+</script>
+<style scoped lang="scss">
+  .feed {
+    &:after {
+      display: block;
+      content: "";
+      clear: both;
+    }
+    div {
+      width: 48.5%;
+      vertical-align: top;
+      a {
+        margin-top: 20px;
+        width: 100%;
+        img {
+          width: 100%;
+        }
+      }
+    }
+    .left-feed {
+      float: left;
+    }
+    .right-feed {
+      float: right;
+    }
+  }
+  .back-btn {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+    background: url("~@/assets/images/back_btn.png") no-repeat center / 24px
+      24px;
+  }
+  .search-area {
+    background: #f2f4f5;
+    padding: 12px 20px;
+    border-radius: 8px;
+    width: calc(100% - 24px);
+    box-sizing: border-box;
+    display: inline-block;
+    .search-input {
+      width: calc(100% - 24px);
+      border: 0;
+      background: #f2f4f5;
+      &::placeholder {
+        color: #9ea7ad;
+      }
+    }
+    .delete-btn {
+      width: 24px;
+      height: 20px;
+      text-align: right;
+      text-indent: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      background: url("~@/assets/images/clear.png") no-repeat right / 20px 20px;
+    }
+  }
+  .search-tab {
+    position: absolute;
+    top: 45px;
+    width: 100%;
+    z-index: 4;
+    background: white;
+    height: 100%;
+    .tab-btn {
+      width: 33.333%;
+      color: #9ea7ad;
+      font-weight: 400;
+    }
+    .active-tab-btn {
+      font-weight: 700;
+      color: #303538;
+    }
+  }
+</style>
