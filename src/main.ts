578f58cfdd63b225fbeb40985303543df1487298
duatpwnd <duatpwnd@snaptag.co.kr>
2021-10-12 11:13:17 +0900

    개발 api서버 임시 url 추가, axios baseurl 설정, apiurl 추가

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,14 @@ const app = createApp(App);
 app.use(store).use(router).mount("#app");
 app.config.globalProperties.axios = axios;
 app.config.globalProperties.apiUrl = apiUrl;
+console.log(process.env);
 app.config.errorHandler = (err, vm, info) => {
   console.log("에러:", err, "vm", vm, "info:", info);
 };
 app.config.warnHandler = (msg, vm, trace) => {
   console.log("경고:", msg, vm, trace);
 };
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 axios.interceptors.request.use(
   (config) => {
     console.log("axios 요청전:", config);
