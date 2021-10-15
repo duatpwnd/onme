061c568e6c67a14fb874d7c1e41927fb342c9e5d
duatpwnd <duatpwnd@snaptag.co.kr>
2021-10-15 18:51:41 +0900

    props 변수 수정

--- src/components/common/BaseCheckBox.vue
+++ src/components/common/BaseCheckBox.vue
@@ -2,14 +2,22 @@
   <span class="base-checkbox">
     <label class="container-checkbox">
       <input
-        :id="fieldId"
+        v-if="value == 'all'"
+        :id="value"
+        type="checkbox"
+        :checked="allChecked"
+        @click="allCheck()"
+      />
+      <input
+        v-else
+        :id="value"
         @input="checkFunc()"
         type="checkbox"
         :checked="checked"
       />
       <span class="checkmark"></span>
     </label>
-    <label :for="fieldId" class="field-id">{{ fieldId }}</label>
+    <label :for="value" class="field-id">{{ fieldId }}</label>
   </span>
 </template>
 <script lang="ts">
@@ -17,9 +25,11 @@
   export default defineComponent({
     name: "BaseCheckBox",
     props: {
+      allChecked: {
+        type: Boolean,
+      },
       modelValue: {
         type: Array,
-        required: true,
       },
       value: {
         type: String,
@@ -30,7 +40,12 @@
     },
     setup(props: { [key: string]: any }, context) {
       const checked = computed(() => props.modelValue.includes(props.value));
+      const allCheck = () => {
+        context.emit("all-check", !props.allChecked);
+      };
       const checkFunc = () => {
+        console.log(checked.value, props.modelValue);
+
         const updatedValue = props.modelValue;
         if (checked.value) {
           const index = updatedValue.indexOf(props.value);
@@ -40,7 +55,7 @@
         }
         context.emit("update:modelValue", updatedValue);
       };
-      return { checked, checkFunc };
+      return { checked, checkFunc, allCheck };
     },
   });
 </script>
@@ -58,6 +73,8 @@
     width: 20px;
     height: 20px;
     display: inline-block;
+    border-radius: 4px;
+
     background: url("~@/assets/images/check_ico.png") #ccc no-repeat center /
       12px 9px;
   }
