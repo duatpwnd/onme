b91e5edd80a47bcc3f9876f63b49e0abea5473df
duatpwnd <duatpwnd@snaptag.co.kr>
2021-10-22 19:12:51 +0900

    나의정보 api연동

--- src/views/MyInfo.vue
+++ src/views/MyInfo.vue
@@ -1,9 +1,35 @@
 <template>
+  <div class="mask" v-show="nicknameChangeModal" @click="initialize()">
+    <div class="nickname-change-modal" @click.stop>
+      <div class="body">
+        <div class="header">
+          <b>닉네임</b
+          ><span class="character-count">({{ nickname.length }}/20)</span>
+          <button @click="nicknameChange('default')" class="save-btn">
+            저장
+          </button>
+        </div>
+        <input
+          type="text"
+          class="input"
+          :value="nickname"
+          v-on:input="nickname = $event.target.value"
+          :oninput="maxlengthCheck()"
+          placeholder="이름 또는 필명 입력"
+        />
+      </div>
+      <p class="error-message" v-show="errorMessage != null">
+        {{ errorMessage }}
+      </p>
+    </div>
+  </div>
   <BaseBottomModal v-show="menu" @close="menu = false">
     <template #button>
       <label for="upload">사진 선택</label>
       <input type="file" id="upload" ref="file" @change="handleFileUpload()" />
-      <button class="basic-btn">기본 이미지</button>
+      <button class="basic-btn" @click="handleFileUpload('default')">
+        기본 이미지
+      </button>
     </template>
   </BaseBottomModal>
   <div class="wrap">
@@ -16,21 +42,24 @@
         <img
           class="profile-img"
           :src="
-            imageUrl == ''
-              ? require('@/assets/images/profile_big_img.png')
-              : imageUrl
+            userInfo.image_profile == null
+              ? require('@/assets/images/signout_profile_img2.png')
+              : userInfo.image_profile
           "
         />
         <button class="profile-change-btn" @click="menu = true"></button>
       </div>
       <div class="nickname-area">
-        <span class="nickname">snaptag</span>
-        <button class="nickname-change-btn"></button>
+        <span class="nickname"> {{ userInfo.username }} </span>
+        <button
+          class="nickname-change-btn"
+          @click="nicknameChangeModal = true"
+        ></button>
       </div>
     </div>
     <section class="my-account">
       <h2 class="h2-title">내 계정</h2>
-      <section>
+      <!-- <section>
         <h3 class="h3-title">연동계정</h3>
         <input
           type="text"
@@ -38,15 +67,15 @@
           placeholder="애플 로그인"
           readonly
         />
-      </section>
+      </section> -->
       <section>
         <h3 class="h3-title">이메일</h3>
-        <input type="text" placeholder="duatpwnd1@naver.com" readonly />
+        <input type="text" :placeholder="userInfo.email" readonly />
       </section>
-      <section>
+      <!-- <section>
         <h3 class="h3-title">비밀번호</h3>
         <input type="password" value="asdlaskdklasd" readonly />
-      </section>
+      </section> -->
     </section>
   </div>
 </template>
@@ -54,9 +83,11 @@
   import {
     defineComponent,
     onMounted,
-    reactive,
+    computed,
     ref,
     getCurrentInstance,
+    toRefs,
+    reactive,
   } from "vue";
   import BaseBottomModal from "@/components/common/BaseBottomModal.vue";
   export default defineComponent({
@@ -70,44 +101,93 @@
         getCurrentInstance()?.appContext.config.globalProperties;
       const axios = globalProperties?.axios;
       const apiUrl = globalProperties?.apiUrl;
-      const store = globalProperties?.store;
+      const store = globalProperties?.$store;
+      const res = reactive({ userInfo: "" });
       const menu = ref(false);
-      const file = ref();
-      const imageUrl = ref("");
-      const handleFileUpload = async () => {
-        const formData = new FormData();
-        const url = URL.createObjectURL(file.value.files[0]);
-        imageUrl.value = url;
-        menu.value = false;
-        // formData.append('user', JSON.stringify(body['user']));
-        formData.append("image_profile", file.value.files[0]);
-        formData.append("username", "test");
+      const file = ref(null);
+      const errorMessage = ref(null);
+      const imageUrl = ref(null);
+      const nickname = ref("");
+      const nicknameChangeModal = ref(false);
+      // 초기화
+      const initialize = () => {
+        nickname.value = "";
+        nicknameChangeModal.value = false;
+        errorMessage.value = null;
+      };
+      // 닉네임변경
+      const nicknameChange = () => {
         axios
-          .post(apiUrl.test, formData, {
+          .patch(apiUrl.profileNicknameChange, { username: nickname.value })
+          .then((result: { [key: string]: any }) => {
+            console.log("닉네임수정결과:", result.data.data.username);
+            initialize();
+            getMyInfo();
+          })
+          .catch((err: any) => {
+            console.log("닉네임수정에러:", err);
+            errorMessage.value = err.data.message;
+          });
+      };
+      // 이미지 업로드
+      const handleFileUpload = async (type: string) => {
+        const formData = new FormData();
+        const inputFile = file.value as unknown as HTMLFormElement;
+        menu.value = false;
+        if (type == "default") {
+          console.log("기본", type);
+          formData.append("image_profile", "");
+        } else {
+          formData.append("image_profile", inputFile.files[0]);
+        }
+        axios
+          .patch(apiUrl.profileImageChange, formData, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
           })
           .then((result: { [key: string]: any }) => {
             console.log("프로필이미지수정결과:", result);
+            getMyInfo();
+          })
+          .catch((err: any) => {
+            console.log("파일업로드에러:", err);
           });
       };
       // 마이페이지 조회
       const getMyInfo = () => {
         axios.get(apiUrl.getMyInfo).then((result: { [key: string]: any }) => {
-          console.log(result);
+          console.log("마이페이지호회결과:", result);
+          res.userInfo = result.data.data;
         });
       };
+      const maxlengthCheck = () => {
+        if (nickname.value.length > 20) {
+          nickname.value = nickname.value.slice(0, 20);
+        }
+      };
+      const { userInfo } = toRefs(res);
       onMounted(() => {
-        console.log("onmounted호출");
         getMyInfo();
       });
-      return { menu, file, imageUrl, handleFileUpload };
+      return {
+        maxlengthCheck,
+        menu,
+        file,
+        imageUrl,
+        nickname,
+        userInfo,
+        nicknameChangeModal,
+        errorMessage,
+        nicknameChange,
+        handleFileUpload,
+        initialize,
+      };
     },
   });
 </script>
 <style scoped lang="scss">
-  .mask {
+  .modal {
     input[type="file"] {
       display: none;
     }
@@ -121,6 +201,60 @@
     }
     .basic-btn {
       margin-top: 40px;
+    }
+  }
+  .mask {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    z-index: 5;
+    .nickname-change-modal {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      .body {
+        background: white;
+        padding: 20px;
+        height: 192px;
+        box-sizing: border-box;
+        .header {
+          b {
+            font-size: 18px;
+          }
+          .character-count {
+            color: #9ea7ad;
+            font-size: 18px;
+            margin-left: 4px;
+          }
+          .save-btn {
+            float: right;
+            color: #256ee8;
+            font-size: 14px;
+          }
+        }
+        .input {
+          width: 150px;
+          margin: 0 auto;
+          font-size: 18px;
+          margin-top: 50px;
+          border: 0;
+          display: block;
+        }
+      }
+      .error-message {
+        background: rgba(13, 16, 26, 0.7);
+        font-size: 14px;
+        color: white;
+        padding: 16px 20px;
+
+        &::before {
+          content: url("~@/assets/images/caution_ico.png");
+          vertical-align: middle;
+          margin-right: 8px;
+        }
+      }
     }
   }
   .wrap {
