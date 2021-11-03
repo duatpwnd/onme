618e34781c3072765db6dabc5ba578511a247e43
duatpwnd <duatpwnd@snaptag.co.kr>
2021-11-03 15:41:28 +0900

    내자산이 0개일때 등록버튼 추가

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -33,30 +33,22 @@
           "
         />
         <span class="user-name">{{ userInfo.data.username }}</span>
-        <!-- <span class="sns-link">
-          <img src="@/assets/images/facebook_ico1.png" class="ico1" />
-          <img src="@/assets/images/facebook_ico2.png" class="ico2" />
-        </span> -->
       </div>
       <p class="guide-msg">
         <strong>{{ userInfo.data.count_posts }}개</strong>의 자산을<br />보호중입니다.
       </p>
     </div>
     <!-- 유저 정보 :: E -->
-    <!-- 0개의 자산일 경우 -->
-    <!-- <div class="register-area" v-if="feedList.length == 0">
-      <p class="msg">보호중인 자산이 없습니다.</p>
-      <button class="register-btn">내 자산 등록</button>
-    </div> -->
-
-    <!-- <img src="@/assets/images/feed1.png" /> -->
+  </div>
+  <div class="register-area" v-if="userInfo.data.count_posts == 0">
+    <p class="msg">보호중인 자산이 없습니다.</p>
+    <label class="register-btn" for="upload">내 자산 등록</label>
   </div>
   <MasnoryLayout v-if="masnory" :id="userInfo.data.id" />
 </template>
 <script lang="ts">
   import {
     defineComponent,
-    onMounted,
     computed,
     ref,
     getCurrentInstance,
@@ -216,17 +208,6 @@
           margin-left: 16px;
           vertical-align: middle;
         }
-        .sns-link {
-          position: absolute;
-          right: 0;
-          top: 0;
-          bottom: 0;
-          margin: auto;
-          height: 24px;
-          .ico1 {
-            margin-right: 16px;
-          }
-        }
       }
       .guide-msg {
         margin-top: 16px;
@@ -251,22 +232,26 @@
         }
       }
     }
-    .register-area {
-      margin-top: 30px;
+  }
+  .register-area {
+    text-align: center;
+    position: absolute;
+    width: 100%;
+    max-width: 435px;
+    top: calc(50vh - 50px);
+    .msg {
+      color: #79828a;
+      font-weight: 700;
+    }
+    .register-btn {
+      display: inline-block;
+      background: black;
+      color: white;
       text-align: center;
-      .msg {
-        color: #79828a;
-        font-weight: 700;
-      }
-      .register-btn {
-        background: black;
-        color: white;
-        text-align: center;
-        width: 80%;
-        padding: 16px 0;
-        border-radius: 4px;
-        margin-top: 24px;
-      }
+      width: 80%;
+      padding: 16px 0;
+      border-radius: 4px;
+      margin-top: 24px;
     }
   }
 </style>
